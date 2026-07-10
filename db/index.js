1262cdb00e89679fa9db0b5c40de5a0a0c418c75
atetoon <i.am.dhruvpandey@gmail.com>
2026-07-10 23:53:08 +0530

    Enable SSL for Render PostgreSQL

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 module.exports = pool;
